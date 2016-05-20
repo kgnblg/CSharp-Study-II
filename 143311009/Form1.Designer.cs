0cd499d482c552a435728708e19ef3098448134a
kgnblg <kgnblg@gmail.com>
2016-05-20 19:58:57 +0300

    a

---
+++ 143311009/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace _143311009
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Basamak Sayısı =";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "B A Ş L A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(357, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 8);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(273, 8);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(36, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(231, 8);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(36, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(189, 8);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(36, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(189, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(231, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 12);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Gold;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(273, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 12);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Gold;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(315, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 12);
+            this.label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Gold;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(357, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 12);
+            this.label7.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(106, 78);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hak Sayısı =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(60, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "1 Hak = 20 Saniye";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(399, 7);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(118, 19);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Süre:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(399, 29);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(118, 19);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Kalan Hak:";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(138, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Rakamlar Tekrarlı Olsun";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(146, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Rakamlar Tekrarsız Olsun";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(417, 133);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(100, 13);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "İsim: Kağan (5 Harf)";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(5, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 149);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 155);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "143311009 - Ödev7";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 143311009/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _143311009
+{
+    public partial class Form1 : Form
+    {
+        int[] dizi = new int[5];
+        int sure = 0;
+        int basamak = 0;
+        int hak = 0;
+        int sayac = 0;
+        bool kontrol1 = true, kontrol2 = true, kontrol3 = true, kontrol4 = true, kontrol5 = true;
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult kagan = MessageBox.Show("Devam etmek için Evet'i, Oyunu kapatmak için Hayır'ı Tıklayınız.","Çıkış Uyarısı",MessageBoxButtons.YesNo);
+            if (kagan == DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if ((int)numericUpDown1.Value == 2)
+            {
+                textBox3.Enabled = false;
+            }else if ((int)numericUpDown1.Value == 3)
+            {
+                textBox3.Enabled = true;
+                textBox4.Enabled = false;
+                textBox5.Enabled = false;
+            }
+            else if ((int)numericUpDown1.Value == 4)
+            {
+                textBox4.Enabled = true;
+                textBox5.Enabled = false;
+            }
+            else if ((int)numericUpDown1.Value == 5)
+            {
+                textBox5.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            basamak = (int)numericUpDown1.Value;
+            hak = (int)numericUpDown2.Value;
+            sure = 20 * hak;
+
+
+            if (radioButton1.Checked)
+            {
+                for (int i = 0; i < basamak; i++)
+                {
+                    dizi[i] = rnd.Next(1, 10);
+                }
+            }
+            else
+            {
+                dizi[0] = rnd.Next(1, 10);
+                for (int i = 1; i < dizi.Length; i++)
+                {
+                    dizi[i] = rnd.Next(1, 10);
+                    for (int k = 0; k < i; k++)
+                    {
+                        while(dizi[i] == dizi[k])
+                        {
+                            dizi[i] = rnd.Next(1, 10);
+                        }
+                    }
+                }
+            }
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label9.Text = "Süre: " + sure;
+            label10.Text = "Hak Sayısı: " + hak;
+            String [] cvp = {textBox1.Text,textBox2.Text,textBox3.Text,textBox4.Text,textBox5.Text};
+            sure--;
+            if (sure == 0 || hak == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("BİLEMEDİNİZ, OYUN BİTTİ - SAYI: "+(sayac*10).ToString());
+            }
+
+
+            if (textBox1.Text != "")
+            {
+                if (int.Parse(textBox1.Text) == dizi[0])
+                {
+                    label7.BackColor = Color.Blue;
+                    sayac++;
+                }
+                else
+                {
+                    if (kontrol1 == true)
+                    {
+                        hak--;
+                        sure -= 20;
+                        label7.BackColor = Color.Gold;
+                        kontrol1 = false;
+                    }
+                }
+
+                for (int i = 1; i < basamak; i++)
+                {
+                    if(int.Parse(cvp[0]) == dizi[i])
+                    {
+                        label7.BackColor = Color.Red;
+                    }
+                }
+            }
+
+            if (textBox2.Text != "")
+            {
+                if (int.Parse(textBox2.Text) == dizi[1])
+                {
+                    label6.BackColor = Color.Blue;
+                    sayac++;
+                }
+                else
+                {
+                    if (kontrol2 == true)
+                    {
+                        hak--;
+                        sure -= 20;
+                        label6.BackColor = Color.Gold;
+                        kontrol2 = false;
+                    }
+                }
+
+                for (int i = 0; i < basamak; i++)
+                {
+                    if (i != 1)
+                    {
+                        if (int.Parse(cvp[1]) == dizi[i])
+                        {
+                            label6.BackColor = Color.Red;
+                        }
+                    }
+                }
+            }
+
+
+            if (basamak == 3)
+            {
+                if (textBox3.Text != "")
+                {
+                    if (int.Parse(textBox3.Text) == dizi[2])
+                    {
+                        label5.BackColor = Color.Blue;
+                        sayac++;
+                    }
+                }
+                else
+                {
+                    if (kontrol3 == true)
+                    {
+                        hak--;
+                        sure -= 20;
+                        label5.BackColor = Color.Gold;
+                        kontrol3 = false;
+                    }
+                }
+
+                for (int i = 0; i < basamak; i++)
+                {
+                    if (i != 2)
+                    {
+                        if (int.Parse(cvp[2]) == dizi[i])
+                        {
+                            label5.BackColor = Color.Red;
+                        }
+                    }
+                }
+            }
+
+            if (basamak == 4)
+            {
+                if (textBox3.Text != "")
+                {
+                    if (int.Parse(textBox3.Text) == dizi[2])
+                    {
+                        label5.BackColor = Color.Blue;
+                        sayac++;
+                    }
+                    else
+                    {
+                        if (kontrol3 == true)
+                        {
+                            hak--;
+                            sure -= 20;
+                            label5.BackColor = Color.Gold;
+                            kontrol3 = false;
+                        }
+                    }
+
+                    for (int i = 0; i < basamak; i++)
+                    {
+                        if (i != 2)
+                        {
+                            if (int.Parse(cvp[2]) == dizi[i])
+                            {
+                                label5.BackColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+
+                if (textBox4.Text != "")
+                {
+                    if (int.Parse(textBox4.Text) == dizi[3])
+                    {
+                        label4.BackColor = Color.Blue;
+                        sayac++;
+                    }
+                    else
+                    {
+                        if (kontrol4 == true)
+                        {
+                            hak--;
+                            sure -= 20;
+                            label4.BackColor = Color.Gold;
+                            kontrol4 = false;
+                        }
+                    }
+
+                    for (int i = 0; i < basamak; i++)
+                    {
+                        if (i != 3)
+                        {
+                            if (int.Parse(cvp[3]) == dizi[i])
+                            {
+                                label4.BackColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (basamak == 5)
+            {
+                if (textBox5.Text != "")
+                {
+                    if (int.Parse(textBox5.Text) == dizi[4])
+                    {
+                        label3.BackColor = Color.Blue;
+                        sayac++;
+                    }
+                    else
+                    {
+                        if (kontrol5 == true)
+                        {
+                            hak--;
+                            sure -= 20;
+                            label3.BackColor = Color.Gold;
+                            kontrol5 = false;
+                        }
+                    }
+
+                    for (int i = 0; i < basamak; i++)
+                    {
+                        if (i != 4)
+                        {
+                            if (int.Parse(cvp[4]) == dizi[i])
+                            {
+                                label3.BackColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+
+                if (textBox3.Text != "")
+                {
+                    if (int.Parse(textBox3.Text) == dizi[2])
+                    {
+                        label5.BackColor = Color.Blue;
+                        sayac++;
+                    }
+                    else
+                    {
+                        if (kontrol3 == true)
+                        {
+                            hak--;
+                            sure -= 20;
+                            label5.BackColor = Color.Gold;
+                            kontrol3 = false;
+                        }
+                    }
+
+                    for (int i = 0; i < basamak; i++)
+                    {
+                        if (i != 2)
+                        {
+                            if (int.Parse(cvp[2]) == dizi[i])
+                            {
+                                label5.BackColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+
+                if (textBox4.Text != "")
+                {
+                    if (int.Parse(textBox4.Text) == dizi[3])
+                    {
+                        label4.BackColor = Color.Blue;
+                        sayac++;
+                    }
+                    else
+                    {
+                        if (kontrol4 == true)
+                        {
+                            hak--;
+                            sure -= 20;
+                            label4.BackColor = Color.Gold;
+                            kontrol4 = false;
+                        }
+                    }
+
+                    for (int i = 0; i < basamak; i++)
+                    {
+                        if (i != 3)
+                        {
+                            if (int.Parse(cvp[3]) == dizi[i])
+                            {
+                                label4.BackColor = Color.Red;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if(sayac == basamak)
+            {
+                timer1.Stop();
+                MessageBox.Show("TEBRİKLER BİLDİNİZ!");
+            }
+        }
+    }
+}
