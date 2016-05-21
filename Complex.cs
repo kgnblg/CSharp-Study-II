04d736681c179d80f1e875a823ba8e10c4ebd3d5
kgnblg <kgnblg@gmail.com>
2016-05-21 16:24:02 +0300

    Add files via upload

---
+++ Complex.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _143311009
+{
+    class Complex
+    {
+        double gercek;
+        double sanal;
+
+        public Complex(double a, double b)
+        {
+            gercek = a;
+            sanal = b;
+        }
+
+        public double Gercek
+        {
+            get
+            {
+                return gercek;
+            }
+
+            set
+            {
+                gercek = value;
+            }
+        }
+
+        public double Sanal
+        {
+            get
+            {
+                return sanal;
+            }
+
+            set
+            {
+                sanal = value;
+            }
+        }
+
+        public static Complex operator true(Complex c1, Complex c2)
+        {
+            return new Complex();
+        }
+
+        public static Complex operator false(Complex c1, Complex c2)
+        {
+            return new Complex();
+        }
+
+        public static Complex operator &(Complex c1, Complex c2)
+        {
+            double p1 = c1.gercek&c2.gercek;
+
+        }
+    }
+}
